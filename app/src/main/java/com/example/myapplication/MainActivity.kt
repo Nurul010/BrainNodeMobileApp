0cebe5f3b8edac0cfbd8a5cbc266e8d67dfd66af
Nurul010 <104237074+Nurul010@users.noreply.github.com>
2022-11-24 14:32:24 -0600

    added home and settings fragment.
    functionality does not fully work yet

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val settingsFragment = SettingsFragment()
+        val navView : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.Frame_Layout, homeFragment)
+            commit()
+        }
+
+        navView.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.Home -> replaceFragment(homeFragment)
+            }
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.Frame_Layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+
+
 }
