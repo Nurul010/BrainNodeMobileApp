4e4ee535ae83ed76f2b9eca83c5c352625ab3d17
Nurul010 <104237074+Nurul010@users.noreply.github.com>
2022-11-25 11:05:03 -0600

    added firstfragment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,20 +19,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val homeFragment = HomeFragment()
+        val firstFragment = FirstFragment()
         val settingsFragment = SettingsFragment()
         val navView : BottomNavigationView = findViewById(R.id.bottomNavigationView)
 
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.Frame_Layout, homeFragment)
+            replace(R.id.Frame_Layout, firstFragment)
             commit()
         }
 
-        navView.setOnNavigationItemReselectedListener {
+        navView.setOnNavigationItemSelectedListener {
             when(it.itemId){
                 R.id.Home -> replaceFragment(homeFragment)
+                R.id.Settings -> replaceFragment(settingsFragment)
             }
+            true
         }
     }
+
 
     private fun replaceFragment(fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
